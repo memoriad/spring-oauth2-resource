3550653abe8b478a59b370002e5cb9e64f88376e
jamikorn jongka <mmemoriad@gmail.com>
2019-10-01 11:15:26 +0700

    first commit

---
+++ src/main/java/co/prior/oauth/demo/OAuth2ResourceApplication.java
@@ -0,0 +1,14 @@
+package co.prior.oauth.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OAuth2ResourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OAuth2ResourceApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/co/prior/oauth/demo/OAuth2ResourceApplicationTests.java
@@ -0,0 +1,17 @@
+package co.prior.oauth.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OAuth2ResourceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
